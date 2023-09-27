4211a2867fa77a399caf9c09058808af283f18f7
AusrineBudzeviciute <145910900+AusrineBudzeviciute@users.noreply.github.com>
2023-09-27 23:11:18 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <cstdio>
 #include <bits/stdc++.h>
+#include <ctime>
+#include <cstdlib>
+
 
 using namespace std;
 
@@ -29,31 +32,60 @@ int main()
         cout<<"Iveskite varda ir pavarde: "<<endl;
         cin>>stud.vardas>>stud.pavarde;
 
-        int m = 0, p;
-        cout<<"Iveskite pazymi (0 zymi pazymiu ivedimo pabaiga)";
-        cin>>p;
-        while (p != 0)
+
+        char ivedimas;
+        int skaicius, p, m=0;
+        cout<<"Iveskite '+', jeigu norite, kad studento pazymius bei egzamino rezultatus sugeneruotu automatiskai. Iveskite '-', jeigu ivesite patys";
+        cin>>ivedimas;
+        if (ivedimas == '+')
         {
-            m++;
-            stud.pazymiai.push_back(p);
+            cout<<"Iveskite, kiek pazymiu generuojama";
+            cin>>skaicius;
+            srand(time(0));
+            for(int j=0;j<skaicius;j++)
+                stud.pazymiai.push_back((rand()%10)+1);
+            sort(stud.pazymiai.begin(), stud.pazymiai.end());
+
+            stud.egz = (rand()%10) + 1;
+
+            if (skaicius % 2 == 0) stud.mediana =( stud.pazymiai[skaicius/2] + stud.pazymiai[(skaicius/2)-1] )/2.0;
+            else stud.mediana = stud.pazymiai[(skaicius/2)];
+
+
+            float suma1=0;
+            for (auto &k: stud.pazymiai)
+                suma1 = suma1 + k;
+            stud.rez = (suma1/skaicius)*0.4 + stud.egz*0.6;
+
+
+        }
+
+        else
+        {
             cout<<"Iveskite pazymi (0 zymi pazymiu ivedimo pabaiga)";
             cin>>p;
+            while (p != 0)
+            {
+                m++;
+                stud.pazymiai.push_back(p);
+                cout<<"Iveskite pazymi (0 zymi pazymiu ivedimo pabaiga)";
+                cin>>p;
+            }
+            sort(stud.pazymiai.begin(), stud.pazymiai.end());
+
+            cout<<"Iveskite egzamino rezultata: "<<endl;
+            cin>>stud.egz;
+
+            if (m % 2 == 0) stud.mediana =( stud.pazymiai[m/2] + stud.pazymiai[(m/2)-1] )/2.0;
+            else stud.mediana = stud.pazymiai[(m/2)];
+
+            float suma2=0;
+            for (auto &k: stud.pazymiai)
+                suma2 = suma2 + k;
+            stud.rez = (suma2/m)*0.4 + stud.egz*0.6;
+
         }
-        sort(stud.pazymiai.begin(), stud.pazymiai.end());
 
-        cout<<"Iveskite egzamino rezultata: "<<endl;
-        cin>>stud.egz;
-
-        float suma=0;
-        for (auto &k: stud.pazymiai)
-            suma = suma + k;
-        stud.rez = (suma/m)*0.4 + stud.egz*0.6;
-
-
-        if (m % 2 == 0)
-            stud.mediana =( stud.pazymiai[m/2] + stud.pazymiai[(m/2)-1] )/2.0;
-        else
-            stud.mediana = stud.pazymiai[(m/2)];
 
         grupe.push_back(stud);
         stud.pazymiai.clear();
@@ -80,9 +112,8 @@ int main()
 
 
 
-
-
 }
+
 
 
 
