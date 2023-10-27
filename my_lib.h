6ec255a539630cea693f52446c035d5aa72ec9bf
AusrineBudzeviciute <145910900+AusrineBudzeviciute@users.noreply.github.com>
2023-10-27 12:50:19 +0300

    Add files via upload

--- my_lib.h
+++ my_lib.h
@@ -18,6 +18,7 @@ using std::cin;
 using std::endl;
 using std::string;
 using std::vector;
+using std::list;
 using std::fixed;
 using std::setprecision;
 using std::setw;
@@ -31,38 +32,35 @@ using std::ofstream;
 using std::to_string;
 using namespace std::chrono;
 
-struct studentas {
-        string vardas, pavarde;
-        vector <int> pazymiai;
-        int egz;
-        float rez, mediana;
+struct stipendininkas {
+    string vardas, pavarde;
+    list<int> pazymiai;
+    int egz;
+    float rez, mediana;
+    int x;
 
-        int x;
-        bool operator<(const studentas& o) const{
-            if(x == 1) return vardas < o.vardas;
-            else if (x == 2) return pavarde < o.pavarde;
-            else if (x == 3) return rez < o.rez;
-        }
-
+    bool operator<(const stipendininkas& o) const {
+        if (x == 1) return vardas < o.vardas;
+        else if (x == 2) return pavarde < o.pavarde;
+        else if (x == 3) return rez < o.rez;
+    }
 };
 
 
-
-
-
-int rusiavimui();
 int generate_random();
-float median(vector<int> pazymiai);
-float mean(vector<int> pazymiai, int egzaminas);
-void print_mean(vector<studentas> grupe);
-void print_median(vector<studentas> grupe);
-void print_mean_median (vector<studentas> grupe);
+int rusiavimui();
+float median(list<int> pazymiai);
+float mean(list<int> pazymiai, int egzaminas);
+void print_mean(list<stipendininkas> kursas);
+void print_median(list<stipendininkas> kursas);
+void print_mean_median (list<stipendininkas> kursas);
 void tikrinimas(int& x);
-void Failo_nuskaitymas (string pavadinimas, struct studentas stud, vector <studentas> &grupe);
+void Failo_nuskaitymas (string pavadinimas, struct stipendininkas stip, list <stipendininkas> &kursas);
 void Failo_kurimas (int studentusk);
-void darbas_su_failu(string pavadinimas, struct studentas stud);
-void print_file(string pavadinimas, vector<studentas> studentai);
-void Failo_rusiavimas (vector <studentas> grupe);
+void Failo_rusiavimas (list <stipendininkas> kursas);
+void print_file(string pavadinimas, list<stipendininkas> studentai);
+
+
 
 
 
