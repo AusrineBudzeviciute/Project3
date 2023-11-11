5f30e8560279efb0aaab12e730bb3042e61c790e
AusrineBudzeviciute <145910900+AusrineBudzeviciute@users.noreply.github.com>
2023-11-11 23:26:41 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,107 +1,23 @@
 #include "my_lib.h"
 
+
 int main()
 {
-try{
-
-    stipendininkas stip;
-    list <stipendininkas> kursas;
-    int ivedimas1;
-    cout<<"Iveskite: '1' - failo nuskaitymui; '2' - duomenu ivedimui; '3' - failo generavimui: ";
-    cin>>ivedimas1;
-
-    if(ivedimas1 == 1)
+    char konteineris;
+    cout<<"Pasirinkite, kokiame konteineryje talpinami duomenys ('v'-vector, 'l'-list): ";
+    cin>>konteineris;
+    if (konteineris == 'l')
     {
-        string pav;
-        cout<<"Iveskite failo pavadinima: ";
-        cin>>pav;
-        Failo_nuskaitymas(pav, stip, kursas);
-        int pasirinkimas = rusiavimui();
-        for (auto &a: kursas)
-            a.x = pasirinkimas;
-        kursas.sort();
-        Failo_rusiavimas(kursas);
+        listas();
     }
-
-    else if (ivedimas1 == 2)
+    else if (konteineris == 'v')
     {
-        int n;
-        cout<<"Iveskite, kiek studentu yra sarase:"<<endl;
-        cin>>n;
-        tikrinimas(n);
-
-        for (int i=0; i<n; i++)
-        {
-            cout<<"Iveskite varda ir pavarde: "<<endl;
-            cin>>stip.vardas>>stip.pavarde;
-
-            char ivedimas2;
-            cout<<"Iveskite: '+' - duomenu generavimui; '-' - duomenu ivedimui: ";
-            cin>>ivedimas2;
-            if (ivedimas2 == '+')
-            {
-                int skaicius;
-                cout<<"Iveskite, kiek pazymiu generuojama: ";
-                cin>>skaicius;
-                tikrinimas(skaicius);
-
-                for(int j=0; j<skaicius; j++) stip.pazymiai.push_back(generate_random());
-                stip.egz = generate_random();
-                stip.mediana = median(stip.pazymiai);
-                stip.rez = mean(stip.pazymiai, stip.egz);
-                kursas.push_back(stip);
-                stip.pazymiai.clear();
-            }
-
-            else if (ivedimas2 == '-')
-            {
-                int p;
-                cout<<"Iveskite pazymi (0 zymi pazymiu ivedimo pabaiga): ";
-                cin>>p;
-
-                if (cin.fail() || p < 0 || p > 10) throw invalid_argument("Pateiktas netinkamas skaicius.");
-
-                while (p != 0)
-                {
-                    stip.pazymiai.push_back(p);
-                    cout<<"Iveskite pazymi (0 zymi pazymiu ivedimo pabaiga): ";
-                    cin>>p;
-                    if (cin.fail() || p < 0 || p > 10) throw invalid_argument("Pateiktas netinkamas skaicius.");
-                }
-
-                cout<<"Iveskite egzamino rezultata: "<<endl;
-                cin>>stip.egz;
-
-                stip.mediana = median(stip.pazymiai);
-                stip.rez = mean(stip.pazymiai, stip.egz);
-                kursas.push_back(stip);
-                stip.pazymiai.clear();
-            }
-        }
-        kursas.sort();
-        char ivedimas3;
-        cout<<"Iveskite: 'V' - vidurkio isvedimui; 'M' - medianos isvedimui: ";
-        cin>>ivedimas3;
-        if (ivedimas3 == 'V') print_mean(kursas);
-        else print_median(kursas);
-        cout<<"Grupes objekto saugojimo atmintyje adresas: "<<&kursas<<endl;
-        for(auto &stip: kursas) {cout<<"Studento objekto saugojimo atmintyje adresas: "<<&stip<<endl;}
-    }
-
-    else if (ivedimas1 == 3)
-    {
-        int studentusk;
-        cout<<"Iveskite, kiek studentu generuojama: ";
-        cin>>studentusk;
-        Failo_kurimas(studentusk);
+        vektorius();
     }
 }
 
-catch (const exception& e) {
-    cerr << "Klaida: " << e.what() << endl;
-    return 1;}
 
-    system("pause");
-    return 0;
 
-}
+
+
+
