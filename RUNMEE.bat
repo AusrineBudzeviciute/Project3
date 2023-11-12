fcc8a309c2c3842fccc7aa01d98fcfe391fada94
AusrineBudzeviciute <145910900+AusrineBudzeviciute@users.noreply.github.com>
2023-11-12 20:08:52 +0200

    Add files via upload

---
+++ RUNMEE.bat
@@ -0,0 +1,9 @@
+cmake CMakeLists.txt
+cmake --install .
+cmake --build .
+copy src\studentai1000.txt Debug\studentai1000.txt
+copy src\studentai10000.txt Debug\studentai10000.txt
+copy src\studentai100000.txt Debug\studentai100000.txt
+copy src\studentai1000000.txt Debug\studentai1000000.txt
+copy src\studentai10000000.txt Debug\studentai10000000.txt
+Debug\Project1.exe
