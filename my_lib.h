f00ad36edd0dbac5142c473cb4a5aea8bd3667c9
AusrineBudzeviciute <145910900+AusrineBudzeviciute@users.noreply.github.com>
2023-11-12 18:21:22 +0200

    Add files via upload

--- my_lib.h
+++ my_lib.h
@@ -31,6 +31,9 @@ using std::exception;
 using std::ofstream;
 using std::to_string;
 using namespace std::chrono;
+using std::stable_partition;
+using std::copy;
+
 
 struct studentas {
         string vardas, pavarde;
@@ -76,7 +79,8 @@ void print_median(vector<studentas> grupe);
 void print_mean_median (vector<studentas> grupe);
 void Failo_nuskaitymas (string pavadinimas, struct studentas stud, vector <studentas> &grupe);
 void Failo_rusiavimas1 (vector <studentas> grupe);
-void Failo_rusiavimas2 (vector<studentas> &grupe);
+void Failo_rusiavimas2 (vector<studentas> grupe);
+void Failo_rusiavimas3 (vector<studentas> grupe);
 void print_file(string pavadinimas, vector<studentas> studentai);
 
 /* LISTAS */
@@ -89,4 +93,5 @@ void print_mean_median_l (list<stipendininkas> kursas);
 void Failo_nuskaitymas_l (string pavadinimas, struct stipendininkas stip, list <stipendininkas> &kursas);
 void Failo_rusiavimas1_l (list <stipendininkas> kursas);
 void Failo_rusiavimas2_l (list<stipendininkas> kursas);
+void Failo_rusiavimas3_l (list<stipendininkas> kursas);
 void print_file_l(string pavadinimas, list<stipendininkas> studentai);
