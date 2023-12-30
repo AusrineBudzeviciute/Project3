ea7cde076120cde237f49c7d8fa7e2468b7cc196
AusrineBudzeviciute <145910900+AusrineBudzeviciute@users.noreply.github.com>
2023-12-30 13:14:06 +0200

    Add files via upload

--- my_lib.h
+++ my_lib.h
@@ -40,58 +40,60 @@ using std::istream;
 using std::ostream;
 
 
-class zmogus {
-    protected:
-        string vardas_, pavarde_;
-        zmogus(){}
 
-    public:
-        string getVardas() const {return vardas_;}
-        string getPavarde() const {return pavarde_;}
-        void setVardas(string);
-        void setPavarde(string);
+class zmogus{
+protected:
+    string vardas_, pavarde_;
+    zmogus() {}
+public:
+    virtual ~zmogus() {}
+    virtual string getVardas() const {return vardas_;}
+    virtual string getPavarde() const {return pavarde_;}
+    virtual void setVardas(string vardas) {vardas_ = vardas;}
+    virtual void setPavarde(string pavarde) {pavarde_ = pavarde;}
+
 };
 
 
 class studentas : public zmogus{
-    private:
-      int egz_;
-      vector<int> pazymiai;
-      float rez_, mediana_;
-      double *elem;
+private:
+    int egz_;
+    vector<int> pazymiai;
+    float rez_, mediana_;
+    double *elem;
+public:
+    studentas() : zmogus(), egz_(0), rez_(0), mediana_(0), elem(nullptr) {}
+    ~studentas(); //destruktorius
+    studentas(const studentas& o); //kopijavimo konstruktorius
+    studentas& operator=(const studentas& o); //priskyrimo operatorius
 
-    public:
-      studentas() : zmogus(), egz_(0), rez_(0), mediana_(0), elem(nullptr) {}
-      ~studentas(); //destruktorius
-      studentas(const studentas& o); //kopijavimo konstruktorius
-      studentas& operator=(const studentas& o); //priskyrimo operatorius
-
-      friend istream& operator>>(istream& is, studentas &s){ //ivesties operatorius
+    friend istream& operator>>(istream& is, studentas &s){ //ivesties operatorius
           string vardas, pavarde;
-          int egz, paz;
-          is>>vardas>>pavarde>>paz>>egz;
+          is>>vardas>>pavarde;
           s.setVardas(vardas);
           s.setPavarde(pavarde);
-          s.setPazymiai(paz);
-          s.setEgz(egz);
           return is;}
 
       friend ostream& operator<<(ostream& os, const studentas &s){ //isvesties operatorius
           os<<left<<setw(20)<<s.getVardas()<<setw(20)<<s.getPavarde();
           return os;}
 
-      vector<int> getPazymiai() const { return pazymiai; }
-      int getEgz() const { return egz_; }
-      float getRez() const { return rez_; }
-      float getMediana() const { return mediana_; }
-
-      void setPazymiai(int);
-      void setEgz(int);
-      void setRez(float);
-      void setMediana(float);
-      void clearPazymiai();
+    string getVardas() const { return zmogus::getVardas(); }
+    string getPavarde() const { return zmogus::getPavarde(); }
+    vector<int> getPazymiai() const { return pazymiai; }
+    int getEgz() const { return egz_; }
+    float getRez() const { return rez_; }
+    float getMediana() const { return mediana_; }
+    void setVardas(string vardas) { zmogus::setVardas(vardas); }
+    void setPavarde(string pavarde) { zmogus::setPavarde(pavarde); }
+    void setPazymiai(int);
+    void setEgz(int);
+    void setRez(float);
+    void setMediana(float);
+    void clearPazymiai();
 
 };
+
 
 
 
@@ -107,7 +109,7 @@ float mean(vector<int> pazymiai, int egzaminas);
 void print_mean(vector<studentas> grupe);
 void print_median(vector<studentas> grupe);
 void print_mean_median (vector<studentas> grupe);
-void Failo_nuskaitymas (string pavadinimas, studentas stud, vector <studentas> &grupe);
+void Failo_nuskaitymas (string pavadinimas, studentas& stud, vector <studentas> &grupe);
 void Failo_kurimas (int studentusk);
 void Failo_rusiavimas (vector<studentas> grupe);
 void print_file(string pavadinimas, vector<studentas> studentai);
